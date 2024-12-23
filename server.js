7076b49675744d8b006398c70f47cdeb4060709c
Cruck Store <cruckstore+github@gmail.com>
2024-12-23 06:08:38 +0100

    very small add if you go to url without any song after it show you all song availible (more simply for know what we have xd)

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
 const soundsPath = path.join(__dirname, 'sounds');
 
 app.use('/sounds', express.static(soundsPath));
 
+app.get('/sounds', (req, res) => {
+  fs.readdir(soundsPath, (err, files) => {
+    if (err) {
+      return res.status(500).send('Error reading files');
+    }
+    
+    const fileLinks = files.map(file => `<li><a href="/sounds/${file}">${file}</a></li>`).join('');
+    const html = `
+      <html>
+        <head>
+          <title>Available Audio Files</title>
+        </head>
+        <body>
+          <h1>Available Audio Files</h1>
+          <ul>
+            ${fileLinks}
+          </ul>
+        </body>
+      </html>
+    `;
+    res.send(html);
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Serveur audio démarré sur http://localhost:${PORT}/sounds`);
+  console.log(`Audio server started at http://localhost:${PORT}/sounds`);
 });
